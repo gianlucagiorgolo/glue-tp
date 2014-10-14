70bb37668d502f00b5c289a4fff3d3f32b21bb18
Gianluca Giorgolo <gianluca.giorgolo@gmail.com>
2014-10-14 12:40:09 +0100

    .

--- DB.hs
+++
@@ -1,4 +0,0 @@
-module DB where
-import TP
-import Control.Monad.State
-test = evalState (toDecorated ([M (Atom "c" :->: Atom "a") :->: Atom "a",Atom "b" :->: ((Atom "a" :->: Atom "b") :->: (Atom "b" :->: Atom "c")),Atom "b" :->: ((Atom "d" :->: Atom "b") :->: Atom "d"),M ((Atom "b" :->: Atom "b") :->: (Atom "b" :->: Atom "a"))],Atom "b") >>= \s -> proofs s) 0
